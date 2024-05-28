2162e652e9f02ba91411346eeb8d54216a367c5f
C.S.T <manuelzela30@gmail.com>
2024-05-28 10:52:15 +0200

    commit admin page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,16 @@ import {HttpClientModule} from "@angular/common/http";
 import {AnimalComponent} from "./pages/home-page/animal/animal.component";
 import {FormsModule} from "@angular/forms";
 import {CarouselModule} from "ngx-bootstrap/carousel";
+import {DogsAdminComponent} from "./pages/admin/dogs-admin/dogs-admin.component";
+import {CatsAdminComponent} from "./pages/admin/cats-admin/cats-admin.component";
+import {BirdsAdminComponent} from "./pages/admin/birds-admin/birds-admin.component";
+import {AdminComponent} from "./pages/admin/admin.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet , HeaderComponent , FooterComponent,HomePageComponent,
-    HttpClientModule,AnimalComponent,FormsModule , CarouselModule],
+    HttpClientModule,AnimalComponent,FormsModule , CarouselModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import {HomePageComponent} from "./pages/home-page/home-page.component";
 import {DogsComponent} from "./pages/dogs/dogs.component";
 import {CatsComponent} from "./pages/cats/cats.component";
 import {BirdsComponent} from "./pages/birds/birds.component";
+import {AdminComponent} from "./pages/admin/admin.component";
+
 
 export const routes: Routes = [
   {path:"" , component:HomePageComponent },
   {path:"dogs" , component:DogsComponent},
   {path:"cats" , component:CatsComponent},
-  {path:"birds", component:BirdsComponent}
+  {path:"birds", component:BirdsComponent},
+  {path:"admin" , component:AdminComponent},
 ];
